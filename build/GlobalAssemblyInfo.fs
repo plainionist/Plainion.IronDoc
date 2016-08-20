1f8fd1376b60241b9f616fc4e7ae0e67c36ce252
ronin4net <stanley751@gmx.net>
2016-08-20 07:06:45 +0200

    GlobalAssemblyInfo.fs added

---
+++ build/GlobalAssemblyInfo.fs
@@ -0,0 +1,14 @@
+﻿namespace Plainion.IronDoc.FSharp
+
+module AssemblyInfo =
+
+    open System
+    open System.Reflection
+    open System.Runtime.InteropServices
+
+    [<assembly: AssemblyDescription( "Plainion.IronDoc" )>]
+    [<assembly: AssemblyCopyright( "Copyright © 2016" )>]
+    [<assembly: AssemblyProduct( "Plainion.IronDoc" )>]
+    [<assembly: AssemblyVersion( "1.2.0.0" )>]
+
+    do()
