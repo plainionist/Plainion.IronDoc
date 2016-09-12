37d5cc9e91586b213adba1e5710680937ace39f7
ronin4net <stanley751@gmx.net>
2016-09-12 12:17:52 +0200

    UTs improved

---
+++ src/Plainion.IronDoc.Tests/Core.fs
@@ -0,0 +1,14 @@
+﻿namespace Plainion.IronDoc.Tests
+
+[<AutoOpen>]
+module Untils =
+    let memoize f = 
+        let cache = ref Map.empty
+        fun x -> 
+            match (!cache).TryFind(x) with
+            | Some res -> res
+            | None -> 
+                 let res = f x
+                 cache := (!cache).Add(x,res)
+                 res
+
--- src/Plainion.IronDoc.Tests/XmlDocToMarkdownTests.fs
+++ src/Plainion.IronDoc.Tests/XmlDocToMarkdownTests.fs
@@ -1,49 +1,55 @@
-﻿namespace Plainion.IronDoc.Tests.Fakes
+﻿namespace Plainion.IronDoc.Tests
 
 open System
 open System.IO
+open System.Collections.Generic
 open NUnit.Framework
 open Plainion.IronDoc.FSharp
 open Plainion.IronDoc.Tests.Fakes
+open Plainion.IronDoc.Tests.Fakes
 
 [<TestFixture>]
-type XmlDocToMarkdownTests() =
-    [<DefaultValue>] val mutable myTransformer : XmlDocTransformer
-    [<DefaultValue>] val mutable myXmlDocumentation : XmlDocDocument.Contents
+module XmlDocToMarkdownTests =
+    
+    let getApiDoc assemblyLocation =
+        let docFile = Path.Combine( Path.GetDirectoryName( assemblyLocation ), Path.GetFileNameWithoutExtension( assemblyLocation ) + ".xml" )
 
-    [<OneTimeSetUpAttribute>]
-    member this.FixtureSetUp() =
+        XmlDocDocument.LoadFile docFile 
+
+    let getApiDocCached = 
+        memoize ( fun t -> getApiDoc t )
+
+    let getTransformer () =
         let loader = new AssemblyLoader()
+        new XmlDocTransformer( loader )
+       
+    let getTransformerCached = 
+        let transformer = getTransformer
+        fun () -> transformer
 
-        this.myTransformer <- new XmlDocTransformer( loader )
-
-        let assembly = this.GetType().Assembly
-        let docFile = Path.Combine( Path.GetDirectoryName( assembly.Location ), Path.GetFileNameWithoutExtension( assembly.Location ) + ".xml" )
-
-        this.myXmlDocumentation <- XmlDocDocument.LoadFile docFile 
-        ()
-
-    member private this.Transform t =
+    let transform ( t : Type ) =
         use writer = new StringWriter()
         
-        this.myTransformer.TransformType t this.myXmlDocumentation writer
+        let apiDoc = getApiDocCached t.Assembly.Location
+        let transformer = getTransformer ()
+        transformer.TransformType t apiDoc writer
         
         writer.ToString();
 
     [<Test>]
-    member this.SimpleSummary () = 
-        let markdownDocument = this.Transform typedefof<SimplePublicClass>
+    let SimpleSummary () = 
+        let markdownDocument = transform typedefof<SimplePublicClass>
 
         Assert.That( markdownDocument, Does.Contain "This is a summary" )
 
     [<Test>]
-    member this.OverwrittenMethods() =
-        let markdownDocument = this.Transform typedefof<OverwrittenMethods>
+    let OverwrittenMethods() =
+        let markdownDocument = transform typedefof<OverwrittenMethods>
 
         Assert.That( markdownDocument, Does.Contain @"Returns nicely formatted message about the state of this object" )
 
     [<Test>]
-    member this.NestedTypes() =
-        let markdownDocument = this.Transform typedefof<NestedType.Nested>
+    let NestedTypes() =
+        let markdownDocument = transform typedefof<NestedType.Nested>
 
         Assert.That( markdownDocument, Does.Contain @"I am nested" )
